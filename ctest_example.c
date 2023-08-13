c2d10cf1426c241fc5aebc0511d36e4708171866
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-08-13 16:54:00 +0100

    feat: don't stop after first failure

--- ctest_example.c
+++ ctest_example.c
@@ -2,27 +2,35 @@
 
 #include "ctest.h"
 
-int foo = 7;
-int bar = 4;
+int foo = 1;
+int bar = 2;
+int baz = 3;
 
 int test_foo() {
-    ctest_assert(foo == 7);
+    ctest_assert(foo == 99);
     return 0;
 }
+
 int test_bar() {
-    ctest_assert(bar == 5);
+    ctest_assert(bar == 2);
+    return 0;
+}
+
+int test_baz() {
+    ctest_assert(baz == 99);
     return 0;
 }
 
 int all_tests() {
     ctest_run_test(test_foo);
     ctest_run_test(test_bar);
+    ctest_run_test(test_baz);
     return 0;
 }
 
 int main(int argc, char** argv) {
-    int result = all_tests();
+    all_tests();
     ctest_report();
 
-    return result != 0;
+    return CTEST_EXIT_CODE;
 }
